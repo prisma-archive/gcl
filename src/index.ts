112bda1b57d5c29544653f40232342f91a2bc4a9
timsuchanek <tim.suchanek@gmail.com>
2017-08-15 12:02:38 +0200

    add basic gcl -> json parsing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log('Hello World')
+import { parse, buildSchema, graphql } from 'graphql'
+
+const wrapGcl = gcl => `query { config(gcl: ${gcl}) }`
+
+export default function gclToJson(
+  gcl: string,
+  schemaString: string,
+): Promise<any> {
+  const query = wrapGcl(gcl)
+  const schema = buildSchema(schemaString)
+
+  let inputObject: any = null
+  const root = {
+    config(input) {
+      inputObject = input
+      return true
+    },
+  }
+
+  return new Promise((resolve, reject) => {
+    graphql(schema, query, root)
+      .then(() => {
+        resolve(inputObject)
+      })
+      .catch(reject)
+  })
+}
